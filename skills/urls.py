5e81d292ca17ab4b95bac717da6bceb0da3f1342
De Xian <ahx.agent007@gmail.com>
2020-01-16 21:22:01 +0600

    learning basic

--- skills/urls.py
+++ skills/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    #/skills/
     path('', views.index, name='index'),
+    #/skills/list/
     path('list', views.allList, name='allList'),
+    #/skills/1111
+    path('<int:skill_id>', views.singleSkill, name='singleSkill'),
 
 ]
